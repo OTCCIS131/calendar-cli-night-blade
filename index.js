54036bad8558ad7b0eb50ef0b109ee7a5d696a39
Unknown <ro0504008@otc.edu>
2017-09-11 13:25:53 -0500

    end of class

---
+++ index.js
@@ -0,0 +1,15 @@
+const m = require ("moment")
+const _ = require ("lodash")
+const c = require ("chalk")
+const ms = require ("moment-range")
+
+const moment = moment-range.extendMoment(M)
+
+let year = moment().range("year")
+
+_.foreach(array.by("months")), month => {
+    
+
+
+
+}

7624ce1b09eaf49b3abd9c1069dfcb696943328c
Unknown <ro0504008@otc.edu>
2017-09-13 13:30:37 -0500

    derp

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const _ = require('lodash')
 const chalk = require('chalk')
 const MomentRange = require('moment-range')
 
-
+//i = to month length? set array with the months dates to call 1 at a time with for eachvia index
 const moment = MomentRange.extendMoment(M)
 
 let year = moment().range('year')
@@ -17,6 +17,25 @@ _.forEach(Array.from(year.by('months')), month => {
     let lastDay = monthRange.end.day()
     let i = 0
 
+    let now = moment()
+console.log(now.year())
+let year = now.range('year')
+for( let month of year.by('month'))
+    {
+        console.log(month)
+    }
+    let days = Array.from(month.range ('month').by('days'))
+    let paddedDays=_.map(days,day =>{
+            let date = day.date
+            if (day.month() == 9 && day.date()  ==  10){
+                date= chalk.red(date)
+            }
+
+            return _.padEnd(date, 2, ' ')
+
+              })
+              console.log(paddedDays)
+/*
     if (firstDay === 0 && i === 0){
 
         console.log(" 1    2    3    4    5    6    7")
@@ -735,6 +754,12 @@ _.forEach(Array.from(year.by('months')), month => {
 
     console.log(firstDay)
     console.log(lastDay)
-
+*/
     
-})
+
+
+
+
+
+
+  

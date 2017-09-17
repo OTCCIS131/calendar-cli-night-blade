459f93b2fd0fa2e42c1503bf62446b185622bd6d
Unknown <ro0504008@otc.edu>
2017-09-17 05:47:00 -0500

    close

--- index.js
+++ index.js
@@ -2,42 +2,43 @@ const M = require('moment')
 const _ = require('lodash')
 const chalk = require('chalk')
 const MomentRange = require('moment-range')
-
-//i = to month length? set array with the months dates to call 1 at a time with for eachvia index
 const moment = MomentRange.extendMoment(M)
 
 let year = moment().range('year')
 
 _.forEach(Array.from(year.by('months')), month => {
-    console.log(_.pad(month.format('MMMM'), 26, ' '))
-    console.log(' S    M    T    W    Th   F    S')
+    console.log(_.pad(month.format('MMMM'), 60, '*'))
+    console.log("\n")
+    console.log('Sunday Monday  Tuesday  Wednesday Thursday Friday  Saturday')
 
     let monthRange = month.range('month')
     let firstDay = monthRange.start.day()
     let lastDay = monthRange.end.day()
-    let i = 0
 
     let now = moment()
 
-
-console.log(now.year())
 let year = now.range('year')
-for( let month of year.by('month'))
-    {
-        console.log(month)
-    }
+
     let days = Array.from(month.range ('month').by('days'))
-    let paddedDays=_.map(days,day =>{
+    let paddedDays=_.map(days, day =>{
             let date = day.date()
-            
-            if (day.month() == 9 && day.date()  ==  10){
-                date= chalk.red(date)
+            if (day.month() == 8 && day.date()  ==  10){
+              date= chalk.blue(date);
             }
+            else if (day.month() == 10 && day.date()  ==  06){
+              date= chalk.red(date);
+              console.log(" ")
+            }
+           //return _.padStart(date, 4, '  ' )
+            return _.padEnd(date, 2, ' ' )
+        })
 
-            return _.padEnd(date, 2, ' ')
+        for (i = 0; i < month.day(); i++) {
+          paddedDays.unshift('  ');
+      }
 
-              })
-              
-              console.log(paddedDays)
-              
-            })
+      paddedDays = _.chunk(paddedDays, 7);
+      paddedDays.forEach(week => {console.log(week.join("       "))})
+
+      console.log("\n\n")
+})
